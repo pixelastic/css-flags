1a5bd29f61ddbbfa582c59000e6cc57e48c84a70
Pixelastic <tim@pixelastic.com>
2014-09-24 20:36:05 +0200

    Adding mixins for vertical stripes.

--- grunt_tasks/options/watch.js
+++ grunt_tasks/options/watch.js
@@ -1,18 +1,21 @@
 module.exports = {
-  options: {
-    livereload: true
-  },
   css: {
-    files: ['<%= config.app %>/css/*.css'],
+    files: ['<%= config.app %>/css/*.scss'],
     tasks: [
       'newer:sass:build',
       'newer:autoprefixer:build'
-    ]
+    ],
+    options: {
+      livereload: true
+    }
   },
   html: {
     files: ['<%= config.app %>/index.html'],
     tasks: [
       'rsync:build'
-    ]
+    ],
+    options: {
+      livereload: true
+    }
   }
 };
