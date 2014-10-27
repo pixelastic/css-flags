ec9986701a1a15e2e353e0a4cd74f042615cea85
Pixelastic <tim@pixelastic.com>
2014-10-27 01:32:51 +0100

    Adding American Samoa.

--- sass_math.rb
+++ sass_math.rb
@@ -4,6 +4,21 @@ module Sass::Script::Functions
 
   module CustomMath
 
+    def deg(number)
+      Sass::Script::Number.new(number.value * 180 / Math::PI);
+    end
+    Sass::Script::Functions.declare :deg, [:number]
+
+    def rad(number)
+      Sass::Script::Number.new(number.value * Math::PI / 180);
+    end
+    Sass::Script::Functions.declare :rad, [:number]
+
+    def sqrt(number)
+      Sass::Script::Number.new(Math.sqrt(number.value))
+    end
+    Sass::Script::Functions.declare :sqrt, []
+
     def pi()
       Sass::Script::Number.new(Math::PI)
     end
@@ -14,15 +29,31 @@ module Sass::Script::Functions
     end
     Sass::Script::Functions.declare :sin, [:number]
 
+    def asin(number)
+      trig(:asin, number)
+    end
+    Sass::Script::Functions.declare :asin, [:number]
+
     def cos(number)
       trig(:cos, number)
     end
     Sass::Script::Functions.declare :cos, [:number]
 
+    def acos(number)
+      trig(:acos, number)
+    end
+    Sass::Script::Functions.declare :acos, [:number]
+
     def tan(number)
       trig(:tan, number)
     end
     Sass::Script::Functions.declare :tan, [:number]
+
+    def atan(number)
+      trig(:atan, number)
+    end
+    Sass::Script::Functions.declare :atan, [:number]
+
 
     private
 
