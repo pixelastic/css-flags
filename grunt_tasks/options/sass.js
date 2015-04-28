4f66601a90517453f97b4db6d1efb2b1b06382f4
Pixelastic <tim@pixelastic.com>
2015-04-28 17:16:29 +0200

    Using Bourbon (WIP).

--- grunt_tasks/options/sass.js
+++ grunt_tasks/options/sass.js
@@ -1,12 +1,12 @@
 module.exports = {
   options: {
     style: 'expanded',
-    require: 'app/css/flags/sass_math.rb'
+    require: './app/css/flags/sass_math.rb'
   },
   build: {
     files: [{
       expand: true,
-      cwd: 'app/css/flags/css',
+      cwd: 'app/css/',
       src: '*.scss',
       dest: 'dist',
       ext: '.css'
